9555fb1716e5ae34b5e19e41bac742b7a584ccee
Amitjindam7755 <145328964+Amitjindam7755@users.noreply.github.com>
2024-06-06 13:09:42 +0530

    Client/Components/Ui

---
+++ Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Button } from "./button";
+
+export default function Header() {
+  return (
+    <nav className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">
+      <Link to="/"><h2 className="font-bold select-none">CodePen Compiler</h2>
+      </Link>
+      <ul className="flex gap-2">
+        <li>
+          <Link to="/compiler"><Button variant="secondary">Compiler
+          </Button></Link>
+        </li>
+      </ul></nav>
+  )
+}
+
