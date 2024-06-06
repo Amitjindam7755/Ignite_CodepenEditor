fd24f829e0e9cad5bf43a2f59daec8e8bcc8c72b
Amitjindam7755 <145328964+Amitjindam7755@users.noreply.github.com>
2024-06-06 13:25:06 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/ui/Header"
+import Home from "./pages/Home"
+import Compiler from "./pages/Compiler"
+import NotFound from "./pages/NotFound"
+import { ThemeProvider } from "@/components/theme-provider"
+
+function App() {
+ 
+
+  return (
+    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/compiler" element={<Compiler/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </ThemeProvider>
+    </>
+  )
+}
+
+export default App
